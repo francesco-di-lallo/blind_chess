bf2c5e6312ba44949fffbdfe8a317f262c9a9e44
Francesco Di Lallo <63360787+francesco-di-lallo@users.noreply.github.com>
2022-12-18 19:04:50 +0000

    Add files via upload

---
+++ chess_utils.py
@@ -0,0 +1,66 @@
+import chess
+import random
+import os
+
+def start_game():
+    """
+    Returns chess.Board object
+    """
+    return chess.Board()
+
+
+def is_game_over(board: chess.Board):
+    """
+    Trigger function for determining if game is over
+    """
+
+    return board.is_checkmate() or board.is_stalemate() or board.is_repetition() or board.is_insufficient_material() or board.is_fifty_moves()
+
+def make_move(move:str, board: chess.Board):
+
+    try:
+        board.push_san(move)
+
+        os.system('clear')
+    except ValueError:
+        if move == "peek":
+            print(board)
+        else:
+            print("Invalid move")
+
+def bot_move(board):
+    possible_moves = list(board.legal_moves)
+
+    return board.san(random.choice(possible_moves))
+
+def game_summary(board: chess.Board):
+
+    move_list = list(board.move_stack)
+
+    board.reset()
+
+
+    for move in range(len(move_list)//2):
+
+        white_move = board.san(move_list.pop(0))
+
+        board.push_san(white_move)
+
+        black_move = board.san(move_list.pop(0))
+
+        board.push_san(black_move)
+
+        print("{}. {} {}".format(move + 1, white_move, black_move))
+
+    if len(move_list) != 0:
+
+        white_move = board.san(move_list[-1])
+
+        board.push_san(white_move)
+
+        print("{}. {}".format(move + 2, white_move))
+
+    print(board)
+
+if __name__ == '__main__':
+    print("Utility classes for blind chess")
---
+++ cli_blind.py
@@ -0,0 +1,39 @@
+import chess
+import chess_utils
+import os
+
+clear = lambda: os.system('clear')
+
+clear()
+
+board = chess_utils.start_game()
+
+user_colour = chess.WHITE
+bot_colour = not user_colour
+
+scholars = ["e4", "e5", "Qh5", "Nc6", "Bc4", "Nf6", "Qf7"]
+draw_rep = ["e4", "e5", "Ke2", "Ke7",
+            "Ke1", "Ke8", "Ke2", "Ke7",
+            "Ke1", "Ke8", "Ke2", "Ke7"]
+
+while not chess_utils.is_game_over(board):
+
+    if board.turn == user_colour:
+        #user move subprocess
+        user_move = input("User to move:")
+
+        chess_utils.make_move(user_move, board)
+
+    else:
+        #bot move subprocess
+
+        bot_move = chess_utils.bot_move(board)
+
+        chess_utils.make_move(bot_move, board)
+
+        print(bot_move)
+
+
+print("GAME SUMMARY")
+
+chess_utils.game_summary(board)
